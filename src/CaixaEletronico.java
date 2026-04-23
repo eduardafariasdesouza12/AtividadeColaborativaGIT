cd18e49e60ff90131b67648971d2f08be7370550
matrixtato <42388326+matrixtato@users.noreply.github.com>
2026-04-22 21:04:02 -0300

    Criando Caixa eletrônico 4.0 Auxiliado pela IA!

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -36,41 +36,34 @@ public class CaixaEletronico {
 
                 System.out.println("Temos as seguintes notas: 100, 50, 20, 10, 5, 2, 1");
 
-                qtdnotas = valor / 100;
-                System.out.println("Disponível: " + qtdnotas + " Notas de R$ 100,00");
+                diferenca = valor;
 
-                diferenca = valor - (qtdnotas * 100);
-                qtdnotas = (valor / 100) / 50;
+                // Notas de 100
+                System.out.println((diferenca / 100) + " Notas de R$ 100,00");
+                diferenca = diferenca % 100;
 
-                System.out.println("Disponível: " + diferenca / 50 + " Notas de R$ 50,00");
-                diferenca = valor - (qtdnotas * 50);
-                diferenca = valor / 50;
-                qtdnotas = (valor / 100) / 50;
+                // Notas de 50
+                System.out.println((diferenca / 50) + " Notas de R$ 50,00");
+                diferenca = diferenca % 50;
 
-                System.out.println("Disponível: " + diferenca / 20 + " Notas de R$ 20,00");
-                //diferenca = diferenca - (qtdnotas * 20);
-                diferenca = diferenca / 20;
-                qtdnotas = (valor / 50) / 20;
+                // Notas de 20
+                System.out.println((diferenca / 20) + " Notas de R$ 20,00");
+                diferenca = diferenca % 20;
 
-                System.out.println("Disponível: " + diferenca / 10 + " Notas de R$ 10,00");
-                diferenca = diferenca - (qtdnotas * 10);
-                diferenca = diferenca / 10;
-                qtdnotas = (valor / 20) / 10;
+                // Notas de 10
+                System.out.println((diferenca / 10) + " Notas de R$ 10,00");
+                diferenca = diferenca % 10;
 
-                System.out.println("Disponível: " + diferenca / 5 + " Notas de R$ 5,00");
-                diferenca = diferenca - (qtdnotas * 5);
-                diferenca = diferenca / 5;
-                qtdnotas = (diferenca / 10) / 5;
+                // Notas de 5
+                System.out.println((diferenca / 5) + " Notas de R$ 5,00");
+                diferenca = diferenca % 5;
 
-                System.out.println("Disponível: " + diferenca / 2 + " Notas de R$ 2,00");
-                diferenca = diferenca - (qtdnotas * 2);
-                diferenca = diferenca / 2;
-                //qtdnotas = (valor / 5) / 2;
+                // Notas de 2
+                System.out.println((diferenca / 2) + " Notas de R$ 2,00");
+                diferenca = diferenca % 2;
 
-                System.out.println("Disponível: " + diferenca / 1 + " Notas de R$ 1,00");
-                diferenca = diferenca - (qtdnotas * 1);
-                diferenca = diferenca / 1;
-                //qtdnotas = (valor / 2) / 1;
+                // Notas de 1
+                System.out.println(diferenca + " Notas de R$ 1,00");
 
                 break;
             }
