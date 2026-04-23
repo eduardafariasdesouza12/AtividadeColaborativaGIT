9392ac4487748044bfcb7d2547dc499fe4ae5bf7
Tatiana Azevedo <tatiana63466296@alunos.sc.senac.br>
2026-04-22 21:42:18 -0300

    ajustado

--- src/CalculadoraMenu.java
+++ src/CalculadoraMenu.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class CalculadoraComMenu {
     public static void main(String[] args) {
+        //editado por tatiana
 
         Scanner scanner = new Scanner(System.in);
         int num1, num2, opcao, resultado;
